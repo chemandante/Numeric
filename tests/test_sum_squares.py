59f7f1659b521eb3032c7229e3b2fbd944d6cd33
Chemandante <valentin@struchalin.com>
2023-02-07 17:28:07 +0300

    Some comments are extended

--- tests/test_sum_squares.py
+++ tests/test_sum_squares.py
@@ -5,6 +5,7 @@ from sympy.ntheory import divisors
 from sum_squares import decomposeBy4
 
 # Count of permutations for different elements equalities in tuple
+# Indexes: [<number of nonzero components> - 2][<equality bit mask>] (see uses of PERMUTATIONS[][])
 PERMUTATIONS = [
     [12, 6],  # 2-tuples: (a,b,0,0) = 12 perms, (a,a,0,0) = 6 perms
     [24, 12, 12, 4],  # 3-tuples: (a,b,c,0) = 24, (a,a,b,0) = 12, (a,b,b,0) = 12, (a,a,a,0) = 4
@@ -31,40 +32,34 @@ def calcDecompositionsBy4Count(num: int) -> int:
 def countRepresentationsOfDeco4(d: tuple) -> int:
     """
     Returns number of representations of particular 4-square decomposition (see Jacobi's four-square theorem)
-    For example, deconposition of 2 = 1^2 + 1^2 can be represented in 24 ways, including permutations, zeros and
+    For example, decomposition of 2 = 1^2 + 1^2 can be represented in 24 ways, including permutations, zeros and
     negatives:
     (1^2 + 0^2 + 1^2 + 0^2), ((-1)^2 + 1^2 + 0^2 + 0^2), (0^2 + 0^2 + (-1)^2 + (-1)^2) and so on
     Result = 2^(number of nonzero summands) * (number of permutations including zero summands)
     :param d: Tuple of particular decomposition (can have less than 4 components). Must be sorted nonascending.
     :return: Number of representations
     """
-    l = len(d)  # Number of nonzero summands
-    if l == 1:  # There is only 4 permutations for one element, so result = 2 * 4 = 8
+    nzc = len(d)  # Number of nonzero summands
+    if nzc == 1:  # There is only 4 permutations for one element, so result = 2 * 4 = 8
         return 8
     # If there are more than 1 element, we should compare them for equality. For example:
     # all summands are different - (a, b, c, d) has 4! = 24 permutations
     # (a, a, b, c) has 4!/2! = 12, (a, a, a, b) has 4!/3! = 4 and (a, a, b, b) has 4!/(2!*2!) = 6 perms
-    # Out components have this relationship: d[0] >= d[1] >= d[2] >= d[3].
-    # Let leftmost inequality relates to less significant bit (LSB) and rightmost to MSB.
-    # LSB == 1 when d[0] == d[1] and LSB == 0 when d[0] > d[1] (not equal)
-    # We can get number `eq` with 3 bits for 4-tuple, 2 bits for 3-tuple and only one bit for 2-tuple.
-    # This number corresponds to equalities and will be used to calculate count of permutations.
+    # Tuple components have this relationship: d[0] >= d[1] >= d[2] >= d[3].
+    # Let compose a mask number `eq` less significant bit (LSB) of which relates to leftmost inequality
+    # (d[0] >= d[1]) and MSB to rightmost.
+    # bit == 1 when d[i] == d[i+1] and bit == 0 when d[i] > d[i+1] (not equal)
+    # `eq` will contain 3 bits for 4-tuple, 2 bits for 3-tuple and only one bit for 2-tuple.
+    # This number (mask) corresponds to equalities and will be used to calculate count of permutations.
     eq = 0
-    for i in range(0, l - 1):
+    for i in range(0, nzc - 1):
         eq *= 2
         eq += 1 if d[i] == d[i + 1] else 0
 
-    return 2**l * PERMUTATIONS[l - 2][eq]  # From `l` to index in list
+    return 2**nzc * PERMUTATIONS[nzc - 2][eq]  # From `l` to index in list
 
 
 class TestSumSquares(unittest.TestCase):
-    # waterCost: list
-    # heatCost: list
-    # electroCost: list
-    # rc_years: list
-    # rc_dr: list
-    # currMonth: str
-    # currYear: int
     nums: tuple
 
     @classmethod
